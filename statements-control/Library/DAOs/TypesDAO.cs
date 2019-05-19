15c9e68320d013828cc9749a339d9e1344c0dcc5
Leonardo3Morais7 <leonardo3morais7@gmail.com>
2019-05-19 02:14:00 -0300

    Created all DAO classes

---
+++ statements-control/Library/DAOs/TypesDAO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.VOs;
+
+namespace Library.DAOs
+{
+    class TypesDAO : MasterDAO
+    {
+        protected override MasterVO BiuldVO(DataRow dr)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override SqlParameter[] CreateParameters(MasterVO o)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLDelete()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLFirst()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLInsert()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLLast()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLPrevious()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLSearch()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string SQLUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
