304c95d18faf2652525ea4cbc1b0be014547ea92
thalees <thalespereiradejesus@gmail.com>
2019-05-19 13:41:12 -0300

    Created Dashboard - user control

---
+++ statements-control/statements-control/uc_Dashboard.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace statements_control
+{
+    partial class uc_Dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // uc_Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "uc_Dashboard";
+            this.Size = new System.Drawing.Size(760, 542);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
