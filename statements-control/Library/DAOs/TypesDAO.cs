0ffa3074782904177b2746c0bf56842faba93e30
Leonardo3Morais7 <leonardo3morais7@gmail.com>
2019-05-19 22:44:14 -0300

    Implemented class TypesDAO

--- statements-control/Library/DAOs/TypesDAO.cs
+++ statements-control/Library/DAOs/TypesDAO.cs
@@ -11,54 +11,96 @@ namespace Library.DAOs
 {
     class TypesDAO : MasterDAO
     {
-        protected override MasterVO BiuldVO(DataRow dr)
+        protected override MasterVO BiuldVO(DataRow row)
         {
-            throw new NotImplementedException();
+            TypesVO type = new TypesVO();
+
+            type.Id = Convert.ToInt32(row["id"]);
+            type.Description = row["description"].ToString();
+            type.Action = row["action"].ToString();
+
+            return type;
+        }
+        protected override SqlParameter[] CreateParameters(MasterVO vo)
+        {
+            SqlParameter[] parameters = {
+                new SqlParameter("id",(vo as TypesVO).Id),
+                new SqlParameter("description", (vo as TypesVO).Description),
+                new SqlParameter("action", (vo as TypesVO).Action)
+            };
+
+            return parameters;
+        }
+        protected override MasterVO ObjectOrNull(DataTable table)
+        {
+            if (table.Rows.Count == 0)
+                return null;
+            else
+                return BiuldVO(table.Rows[0]);
         }
 
-        protected override SqlParameter[] CreateParameters(MasterVO o)
+
+
+        protected override void SQLInsert(MasterVO vo)
         {
-            throw new NotImplementedException();
+            Methods.SQLNonQueryProcedure("usp_InsertType", CreateParameters(vo));
+        }
+        protected override void SQLUpdate(MasterVO vo)
+        {
+            Methods.SQLNonQueryProcedure("usp_UpdateType", CreateParameters(vo));
+        }
+        protected override void SQLDelete(int primaryKey)
+        {
+            SqlParameter[] parameter = { new SqlParameter("id", primaryKey) };
+            Methods.SQLNonQueryProcedure("usp_DeleteType", parameter);
         }
 
-        protected override string SQLDelete()
+
+        protected override MasterVO SQLFirst()
         {
-            throw new NotImplementedException();
+            DataTable table = Methods.SQLSelectProcedure("usp_FirstType", null);
+            return ObjectOrNull(table);
+        }
+        protected override MasterVO SQLLast()
+        {
+            DataTable table = Methods.SQLSelectProcedure("usp_LastType", null);
+            return ObjectOrNull(table);
+        }
+        protected override MasterVO SQLNext(int primaryKey)
+        {
+            SqlParameter[] parameter = { new SqlParameter("@currentId", primaryKey) };
+            DataTable table = Methods.SQLSelectProcedure("usp_NextType", parameter);
+            return ObjectOrNull(table);
+        }
+        protected override MasterVO SQLPrevious(int primaryKey)
+        {
+            SqlParameter[] parameter = { new SqlParameter("@currentId", primaryKey) };
+            DataTable table = Methods.SQLSelectProcedure("usp_PreviousType", parameter);
+            return ObjectOrNull(table);
         }
 
-        protected override string SQLFirst()
-        {
-            throw new NotImplementedException();
-        }
 
-        protected override string SQLInsert()
+        protected override DataTable SQLSearch(MasterVO vo)
         {
-            throw new NotImplementedException();
-        }
+            List<SqlParameter> parameters = new List<SqlParameter>();
 
-        protected override string SQLLast()
-        {
-            throw new NotImplementedException();
-        }
 
-        protected override string SQLNext()
-        {
-            throw new NotImplementedException();
-        }
+            if ((vo as TypesVO).Id > 0)
+                parameters.Add(new SqlParameter("@id", (vo as TypesVO).Id));
+            else
+                parameters.Add(new SqlParameter("@id", DBNull.Value));
 
-        protected override string SQLPrevious()
-        {
-            throw new NotImplementedException();
-        }
+            if (!string.IsNullOrEmpty((vo as TypesVO).Description))
+                parameters.Add(new SqlParameter("@description", (vo as TypesVO).Description));
+            else
+                parameters.Add(new SqlParameter("@description", DBNull.Value));
 
-        protected override string SQLSearch()
-        {
-            throw new NotImplementedException();
-        }
+            if (!string.IsNullOrEmpty((vo as TypesVO).Action))
+                parameters.Add(new SqlParameter("@action", (vo as TypesVO).Action));
+            else
+                parameters.Add(new SqlParameter("@action", DBNull.Value));
 
-        protected override string SQLUpdate()
-        {
-            throw new NotImplementedException();
+            return Methods.SQLExecuteSelect("usp_SearchType", parameters.ToArray());
         }
     }
 }
