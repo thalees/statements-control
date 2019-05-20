a576469d3cb66d5666b0aa8a86c6876157179a0f
Leonardo3Morais7 <leonardo3morais7@gmail.com>
2019-05-19 22:42:27 -0300

    Implemented class EnvironmentsDAO

--- statements-control/Library/DAOs/EnvironmentsDAO.cs
+++ statements-control/Library/DAOs/EnvironmentsDAO.cs
@@ -11,54 +11,87 @@ namespace Library.DAOs
 {
     class EnvironmentsDAO : MasterDAO
     {
-        protected override MasterVO BiuldVO(DataRow dr)
+        protected override MasterVO BiuldVO(DataRow row)
         {
-            throw new NotImplementedException();
+            EnvironmentsVO environment = new EnvironmentsVO();
+            environment.Id = Convert.ToInt32(row["id"]);
+            environment.Name = row["name"].ToString();
+            return environment;
+        }
+        protected override SqlParameter[] CreateParameters(MasterVO vo)
+        {
+            SqlParameter[] parameters = {
+                new SqlParameter("id",(vo as EnvironmentsVO).Id),
+                new SqlParameter("name", (vo as EnvironmentsVO).Name),
+            };
+
+            return parameters;
+        }
+        protected override MasterVO ObjectOrNull(DataTable table)
+        {
+            if (table.Rows.Count == 0)
+                return null;
+            else
+                return BiuldVO(table.Rows[0]);
         }
 
-        protected override SqlParameter[] CreateParameters(MasterVO environment)
+
+
+        protected override void SQLInsert(MasterVO vo)
         {
-            throw new NotImplementedException();
+            Methods.SQLNonQueryProcedure("usp_InsertEnvironment", CreateParameters(vo));
+        }
+        protected override void SQLUpdate(MasterVO vo)
+        {
+            Methods.SQLNonQueryProcedure("usp_UpdateEnvironment", CreateParameters(vo));
+        }
+        protected override void SQLDelete(int primaryKey)
+        {
+            SqlParameter[] parameter = { new SqlParameter("id", primaryKey) };
+            Methods.SQLNonQueryProcedure("usp_DeleteEnvironment", parameter);
         }
 
-        protected override string SQLDelete()
+
+        protected override MasterVO SQLFirst()
         {
-            throw new NotImplementedException();
+            DataTable table = Methods.SQLSelectProcedure("usp_FirstEnvironment", null);
+            return ObjectOrNull(table);
+        }
+        protected override MasterVO SQLLast()
+        {
+            DataTable table = Methods.SQLSelectProcedure("usp_LastEnvironment", null);
+            return ObjectOrNull(table);
+        }
+        protected override MasterVO SQLNext(int primaryKey)
+        {
+            SqlParameter[] parameter = { new SqlParameter("@currentId", primaryKey) };
+            DataTable table = Methods.SQLSelectProcedure("usp_NextEnvironment", parameter);
+            return ObjectOrNull(table);
+        }
+        protected override MasterVO SQLPrevious(int primaryKey)
+        {
+            SqlParameter[] parameter = { new SqlParameter("@currentId", primaryKey) };
+            DataTable table = Methods.SQLSelectProcedure("usp_PreviousEnvironment", parameter);
+            return ObjectOrNull(table);
         }
 
-        protected override string SQLFirst()
+        protected override DataTable SQLSearch(MasterVO vo)
         {
-            throw new NotImplementedException();
-        }
+            List<SqlParameter> parameters = new List<SqlParameter>();
 
-        protected override string SQLInsert()
-        {
-            throw new NotImplementedException();
-        }
 
-        protected override string SQLLast()
-        {
-            throw new NotImplementedException();
-        }
+            if ((vo as EnvironmentsVO).Id > 0)
+                parameters.Add(new SqlParameter("@id", (vo as EnvironmentsVO).Id));
+            else
+                parameters.Add(new SqlParameter("@id", DBNull.Value));
 
-        protected override string SQLNext()
-        {
-            throw new NotImplementedException();
-        }
+            if(!string.IsNullOrEmpty((vo as EnvironmentsVO).Name))
+                parameters.Add(new SqlParameter("@name", (vo as EnvironmentsVO).Name));
+            else
+                parameters.Add(new SqlParameter("@name", DBNull.Value));
 
-        protected override string SQLPrevious()
-        {
-            throw new NotImplementedException();
-        }
 
-        protected override string SQLSearch()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override string SQLUpdate()
-        {
-            throw new NotImplementedException();
+            return Methods.SQLExecuteSelect("usp_SearchEnvironment", parameters.ToArray());
         }
     }
 }
