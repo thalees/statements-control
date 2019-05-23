fd7b274069874ecbb271928b5899f6d971e47270
Leonardo3Morais7 <leonardo3morais7@gmail.com>
2019-05-22 23:36:35 -0300

    Created the form to insert the Database Connection Parameters

---
+++ statements-control/statements-control/fr_DataBaseParameters.cs
@@ -0,0 +1,82 @@
+﻿using Library;
+using Library.DAOs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace statements_control
+{
+    public partial class fr_DataBaseParameters : Form
+    {
+        public fr_DataBaseParameters()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            errorProvider.Clear();
+
+            bool foundError = false;
+            if (string.IsNullOrEmpty(txtDbCatalog.Text))
+            {
+                foundError = true;
+                errorProvider.SetError(txtDbCatalog, "Digite uma instância válida.");
+            }
+            if (string.IsNullOrEmpty(txtDbUsername.Text))
+            {
+                foundError = true;
+                errorProvider.SetError(txtDbUsername, "Digite um nome válido.");
+            }
+            if (string.IsNullOrEmpty(txtDbPassword.Text))
+            {
+                foundError = true;
+                errorProvider.SetError(txtDbPassword, "Digite uma senha válida.");
+            }
+
+            if (!foundError)
+            {
+                if (ConnectionDB.setCatalogDB(txtDbCatalog.Text, this.GetType()) &&
+                    ConnectionDB.setUsernameDB(txtDbUsername.Text, this.GetType()) &&
+                    ConnectionDB.setPasswordDB(txtDbPassword.Text, this.GetType()))
+                {
+                    MessageBox.Show("Parâmetros do Banco de Dados alterados com sucesso!", "Sucesso",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                    MessageBox.Show("Erro ao alterar parâmetros do Banco de Dados", "Erro!",
+                                     MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_TestConnection_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ConnectionDB.GetConnection())
+                {
+                    btn_TestConnection.IdleForecolor = Color.SeaGreen;
+                    btn_TestConnection.IdleLineColor = Color.SeaGreen;
+                    btn_TestConnection.ActiveFillColor = Color.SeaGreen;
+                }
+            }
+            catch
+            {
+                btn_TestConnection.IdleForecolor = Color.Red;
+                btn_TestConnection.IdleLineColor = Color.Red;
+                btn_TestConnection.ActiveFillColor = Color.Red;
+            }
+        }
+    }
+}
--- statements-control/statements-control/fr_Login.cs
+++ statements-control/statements-control/fr_Login.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Library.DAOs;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,36 @@ namespace statements_control
         public fr_Login()
         {
             InitializeComponent();
+        }
+
+        private void ChangeButtonColor(Bunifu.Framework.UI.BunifuThinButton2 button, Color color)
+        {
+            button.IdleForecolor = color;
+            button.IdleLineColor = color;
+            button.ActiveFillColor = color;
+        }
+        private void btn_Database_Click(object sender, EventArgs e)
+        {
+            fr_DataBaseParameters dataBaseParameters = new fr_DataBaseParameters();
+            dataBaseParameters.ShowDialog();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                using (ConnectionDB.GetConnection())
+                {
+                    ChangeButtonColor(btn_Database, Color.SeaGreen);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Erro ao conectar no Banco de Dados.", "Erro!",
+                     MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ChangeButtonColor(btn_Database, Color.Red);
+            }     
         }
     }
 }
