717d6839d4361268641396c72851c667ab170b24
thalees <thalespereiradejesus@gmail.com>
2019-05-26 16:16:26 -0300

    Created goals CRUD procedures

---
+++ statements-control/Database/Stored Procedures/Goals.sql
@@ -0,0 +1,58 @@
+
+CREATE PROCEDURE usp_InsertGoal(
+    @userId INT,
+    @name VARCHAR(20), 
+    @value DECIMAL(18, 2), 
+	@endDate SMALLDATETIME
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			INSERT INTO Goals (userId, [name], [value], endDate)
+			VALUES (@userId, @name, @value, @endDate)
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE usp_UpdateGoal(
+	@id INT,
+    @userId INT,
+    @name VARCHAR(20), 
+    @value DECIMAL(18, 2),
+	@endDate SMALLDATETIME
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			UPDATE Goals SET
+				userId = @userId, 
+				[name] = @name, 
+				[value] = @value, 
+				endDate = @endDate
+			WHERE id = @id
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+	END CATCH
+END
+GO
+
+CREATE PROCEDURE usp_DeleteGoal (@id INT) AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			DELETE Goals WHERE id = @id
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		ROLLBACK
+	END CATCH
+END
+GO
